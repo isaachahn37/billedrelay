d776723d23c6150e095196fb9bba1e2d9466d7dc
isaachahnepicareer <isaac@epicareer.com>
2024-07-30 17:44:47 +0700

    added logging and fix repository

--- src/main/java/com/isaachahn/billedrelay/repository/PackageAppliedRepository.java
+++ src/main/java/com/isaachahn/billedrelay/repository/PackageAppliedRepository.java
@@ -3,6 +3,8 @@ package com.isaachahn.billedrelay.repository;
 import com.isaachahn.billedrelay.models.entity.PackageApplied;
 import com.isaachahn.billedrelay.models.entity.RentalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,10 @@ import java.util.List;
 @Repository
 public interface PackageAppliedRepository extends JpaRepository<PackageApplied, Long> {
     List<PackageApplied>  findByRentalEntity(RentalEntity rentalEntity);
-    List<PackageApplied> findByAppliedTimeStampBetweenAndRentalEntity(Long greaterThan, Long lessThan, RentalEntity rentalEntity);
+//    List<PackageApplied> findByAppliedTimeStampBetweenAndRentalEntity(Long greaterThan, Long lessThan, RentalEntity rentalEntity);
+    @Query("SELECT p FROM PackageApplied p WHERE p.appliedTimeStamp BETWEEN :startTime AND :endTime AND p.rentalEntity = :rentalEntity")
+    List<PackageApplied> findByAppliedTimeStampBetweenAndRentalEntity(
+            @Param("startTime") Long startTime,
+            @Param("endTime") Long endTime,
+            @Param("rentalEntity") RentalEntity rentalEntity);
 }
